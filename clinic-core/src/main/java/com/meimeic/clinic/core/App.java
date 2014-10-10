160dc270566d5bc66d828ef33cb22219f34544a5
meimeic <zhao.jian.ning66@gmail.com>
2014-10-11 00:07:21 +0800

    first commit

---
+++ clinic-core/src/main/java/com/meimeic/clinic/core/App.java
@@ -0,0 +1,13 @@
+package com.meimeic.clinic.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
